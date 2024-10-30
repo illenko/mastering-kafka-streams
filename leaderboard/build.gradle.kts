1cacaff8667904b9eb713a7cbe01c3cc293188f7
illenko <kostiaillenko@gmail.com>
2024-10-30 07:35:45 +0200

    Add leaderboard project template

---
+++ leaderboard/build.gradle.kts
@@ -0,0 +1,56 @@
+val avroVersion: String by project
+val kafkaAvroSerializerVersion: String by project
+
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+	id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven {
+		url = uri("https://packages.confluent.io/maven/")
+	}
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+
+	implementation("org.apache.kafka:kafka-streams")
+	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.apache.avro:avro:$avroVersion")
+	implementation("io.confluent:kafka-avro-serializer:$kafkaAvroSerializerVersion")
+	implementation("io.confluent:kafka-streams-avro-serde:$kafkaAvroSerializerVersion")
+
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}

7465e81d5c6ac6f4f43211ee310f1d20b22b9828
illenko <kostiaillenko@gmail.com>
2024-11-10 13:53:12 +0200

    Add LRU state store

--- advanced-state-management/src/main/kotlin/com/example/management/streams/ManagementTopology.kt
+++ advanced-state-management/src/main/kotlin/com/example/management/streams/ManagementTopology.kt
@@ -1,12 +1,27 @@
 package com.example.management.streams
 
+import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
+import org.apache.kafka.streams.kstream.Materialized
+import org.apache.kafka.streams.state.Stores
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ManagementTopology {
     @Bean
-    fun topology(builder: StreamsBuilder): Topology = builder.build()
+    fun topology(builder: StreamsBuilder): Topology {
+        builder
+            .stream<String, String>("patient-events")
+            .groupByKey()
+            .count(
+                Materialized
+                    .`as`<String, Long>(Stores.lruMap("counts", 10))
+                    .withKeySerde(Serdes.String())
+                    .withValueSerde(Serdes.Long()),
+            )
+
+        return builder.build()
+    }
 }
